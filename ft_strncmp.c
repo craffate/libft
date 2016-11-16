39ff9ab239864814c87e7efa6e7b7f17534bbd9b
Cyril Raffatelli <craffate@student.42.fr>
2016-11-16 13:54:52 +0100

    Improved ft_strncmp and ft_strsplit

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: craffate <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/04 11:03:28 by craffate          #+#    #+#             */
-/*   Updated: 2016/11/16 12:35:47 by craffate         ###   ########.fr       */
+/*   Updated: 2016/11/16 13:45:32 by craffate         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,16 @@
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	while (*s1 && *s2 && *s1 == *s2 && n > 0)
+	size_t	i;
+
+	i = 0;
+	while (*s1 && *s1 == *s2 && i < n)
 	{
 		s1++;
 		s2++;
-		n--;
+		i++;
 	}
+	if (i == n)
+		return (*(unsigned char *)--s1 - *(unsigned char *)--s2);
 	return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
