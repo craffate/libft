156df0d6a6fcc8467f615dfbeebd0e52ebbcf39d
Cyril Raffatelli <craffate@student.42.fr>
2017-01-14 11:22:55 +0100

    Fixed the case ft_putwchar wouldn't work for some characters

--- srcs/ft_putwchar.c
+++ srcs/ft_putwchar.c
@@ -6,7 +6,7 @@
 /*   By: craffate <craffate@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/09 12:26:50 by craffate          #+#    #+#             */
-/*   Updated: 2017/01/09 12:43:32 by craffate         ###   ########.fr       */
+/*   Updated: 2017/01/14 11:22:25 by craffate         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 void	ft_putwchar(wchar_t c)
 {
-	if (c > 0x10000)
+	if (c >= 0x10000)
 		ft_putchar(((c & 0x1c0000) >> 18) + 0xF0);
-	if (c > 0x800)
+	if (c >= 0x800)
 		ft_putchar(c > 0x10000 ? ((c & 0x03F000) >> 12) + 0x80 :
 		((c & 0xF000) >> 12) + 0xE0);
-	if (c > 0x80)
+	if (c >= 0x80)
 		ft_putchar(c > 0x800 ? ((c & 0x0FC0) >> 6) + 0x80 :
 		((c & 0x07c0) >> 6) + 0xc0);
-	ft_putchar(c > 0x80 ? (c & 0x003F) + 0x80 : c);
+	ft_putchar(c >= 0x80 ? (c & 0x003F) + 0x80 : c);
 }
