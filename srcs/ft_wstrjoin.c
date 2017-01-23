69ed0ce7d41f959d3688241f39a13abc67b8dc68
Cyril Raffatelli <craffate@student.42.fr>
2017-01-23 15:24:02 +0100

    Fixed Norme errors

--- srcs/ft_wstrjoin.c
+++ srcs/ft_wstrjoin.c
@@ -6,7 +6,7 @@
 /*   By: craffate <craffate@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/11 17:37:37 by craffate          #+#    #+#             */
-/*   Updated: 2017/01/11 17:50:39 by craffate         ###   ########.fr       */
+/*   Updated: 2017/01/23 15:23:49 by craffate         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,8 @@ wchar_t	*ft_wstrjoin(const wchar_t *s1, const wchar_t *s2)
 	if (!s1 || !s2)
 		return (NULL);
 	i = 0;
-	if (!(s3 = (wchar_t *)malloc(sizeof(wchar_t) * (ft_wstrlen(s1) + ft_wstrlen(s2) + 1))))
+	if (!(s3 = (wchar_t *)malloc(sizeof(wchar_t) *
+		(ft_wstrlen(s1) + ft_wstrlen(s2) + 1))))
 		return (NULL);
 	while (*s1)
 		s3[i++] = *s1++;
